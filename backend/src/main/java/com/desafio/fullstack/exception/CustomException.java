25a645976bba08107a52bb5a68bfd0aef76a9da1
Matheus <matheusnagot@hotmail.com>
2022-09-23 14:20:43 -0300

    feat:created CustomException

---
+++ backend/src/main/java/com/desafio/fullstack/exception/CustomException.java
@@ -0,0 +1,12 @@
+package com.desafio.fullstack.exception;
+
+public class CustomException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public CustomException(String text) {
+		super(text);
+
+	}
+
+}
